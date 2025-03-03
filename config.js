146bd720a8f59347f204d3d1d6a21928856958de
cdnkr <chad@forgood.co.za>
2025-03-04 01:37:50 +0200

    updates config

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const config = {
     "title": "cdnkr.blog",
-    "description": "some description",
-    "keywords": "some,keywords",
-    "githubPagesUrl": "cdnkr.github.io",
-    "push": false,
-    "commitMessage": "updates"
+    "description": "Just another js bro",
+    "keywords": "frontend, javascript, typescript, react, nextjs, tailwindcss, css, html, web development, web design, web development blog, web design blog, web development tutorial, web design tutorial, web development blog post, web design blog post, web development blog post title, web design blog post title",
 }
 
 export default config;
